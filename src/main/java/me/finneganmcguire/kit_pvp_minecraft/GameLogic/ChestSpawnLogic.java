a72540e916d45be53189525bac964ae89baa8f08
vscala <vincent2000nc@gmail.com>
2021-03-18 21:05:11 -0400

    Added game commands

--- src/main/java/me/finneganmcguire/kit_pvp_minecraft/GameLogic/ChestSpawnLogic.java
+++ src/main/java/me/finneganmcguire/kit_pvp_minecraft/GameLogic/ChestSpawnLogic.java
@@ -1,4 +1,8 @@
 package me.finneganmcguire.kit_pvp_minecraft.GameLogic;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+
 public class ChestSpawnLogic {
 }
--- src/main/java/me/finneganmcguire/kit_pvp_minecraft/GameLogic/GameStartLogic.java
+++ src/main/java/me/finneganmcguire/kit_pvp_minecraft/GameLogic/GameStartLogic.java
@@ -1,10 +1,12 @@
 package me.finneganmcguire.kit_pvp_minecraft.GameLogic;
 
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.World;
+import org.bukkit.command.ConsoleCommandSender;
 
 public class GameStartLogic {
-
+    static final int WORLDSIZE = 500;
     // When Game Starts
     public static void GameStart(World w){
         // Finds all players and teleports them to spawn
@@ -12,5 +14,9 @@ public class GameStartLogic {
             w.getPlayers().get(i).teleport(w.getSpawnLocation());
             w.getPlayers().get(i).setGameMode(GameMode.SURVIVAL);
         }
+
+        ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+        Bukkit.getServer().dispatchCommand(console, "worldborder center 0 0");
+        Bukkit.getServer().dispatchCommand(console, String.format("worldborder set %d", WORLDSIZE));
     }
 }
