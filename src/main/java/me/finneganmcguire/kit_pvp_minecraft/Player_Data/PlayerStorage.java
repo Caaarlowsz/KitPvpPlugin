90847f68747950ccaca7897663c0962aaa1bc4f2
vscala <vincent2000nc@gmail.com>
2021-03-18 20:06:18 -0400

    Changed player storage functionality

--- src/main/java/me/finneganmcguire/kit_pvp_minecraft/Player_Data/PlayerStorage.java
+++ src/main/java/me/finneganmcguire/kit_pvp_minecraft/Player_Data/PlayerStorage.java
@@ -6,24 +6,20 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class PlayerStorage {
-    public static ArrayList<HashMap<String, String>> playerHashData = new ArrayList<HashMap<String, String>>();
+    public static HashMap<String, String> playerData = new HashMap<String, String>();
 
     public static boolean playerHasKitActive(Player player, String kitname){
-        for (int i = 0; i < playerHashData.size(); i++) {
-            if(playerHashData.get(i).containsKey(player.getName()) && playerHashData.get(i).get(player.getName()).equals(kitname)){
-                return true;
-            }
-        }
-
+        if (playerData.containsKey(player.getName())) return playerData.get(player.getName()) == kitname;
+        System.out.println("Error: Player has not been registered!!!");
+        playerData.put(player.getName(), "None");
         return false;
     }
 
     public static void setPlayerNewKit(Player player, String kitname){
-        for (int i = 0; i < playerHashData.size(); i++) {
-            if(playerHashData.get(i).containsKey(player.getName())){
-                playerHashData.get(i).put(player.getName(), kitname);
-                System.out.println("Player Is Now: " + playerHashData.get(i).get(player.getName()) + " Kit");
-            }
-        }
+        playerData.put(player.getName(), kitname);
+    }
+
+    public static void remove(Player player) {
+        playerData.remove(player.getName());
     }
 }
