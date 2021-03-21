d65f6e07456d23abe10dd6147ba653fcde5b22f4
Finnegan McGuire <mcguirefinneganftw@gmail.com>
2021-03-20 23:06:55 -0400

    added compass stuff

--- src/main/java/me/finneganmcguire/kit_pvp_minecraft/GameLogic/CompassTracker.java
+++ src/main/java/me/finneganmcguire/kit_pvp_minecraft/GameLogic/CompassTracker.java
@@ -1,37 +1,44 @@
 package me.finneganmcguire.kit_pvp_minecraft.GameLogic;
 
 import me.finneganmcguire.kit_pvp_minecraft.Kit_PvP_Minecraft;
+import org.bukkit.EntityEffect;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import java.util.List;
+
 public class CompassTracker implements Listener {
-    @EventHandler
-    public void onRightClick(PlayerInteractEvent event){
-        Player player = event.getPlayer();
-        Player target = getNearest(player, Kit_PvP_Minecraft.WORLDSIZE);
-        if((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && player.getItemInHand().getType() == Material.COMPASS){
-            player.setCompassTarget(target.getLocation());
+
+    List<Entity> playersNearbyPlayer;
+    double distanceToPlayer = 0.0;
+
+    public String getNearestPlayertoSelf(Player p){
+        List<Entity> nearestEntities = p.getNearbyEntities(Kit_PvP_Minecraft.WORLDSIZE, Kit_PvP_Minecraft.WORLDSIZE, Kit_PvP_Minecraft.WORLDSIZE);
+        for (int i = 0; i < nearestEntities.size(); i++) {
+                if(nearestEntities.get(i).getType().equals(EntityType.PLAYER)){
+                    Player player = (Player) nearestEntities.get(i);
+                    playersNearbyPlayer.add(player);
+
+                }
         }
+
+        return null;
     }
-    private Player getNearest(Player player, int range) {
-        double distance = Double.POSITIVE_INFINITY; // To make sure the first
-        // player checked is closest
-        Player target = null;
-        for (Entity entity : player.getNearbyEntities(200, 200, 200)) {
-            if (!(entity instanceof Player))
-                continue;
-            if(entity == player) continue; //Added this check so you don't target yourself.
-            double distanceto = player.getLocation().distance(entity.getLocation());
-            if (distanceto > distance)
-                continue;
-            distance = distanceto;
-            target = (Player) entity;
+
+    @EventHandler
+    public void rightClickCompass(PlayerInteractEvent e){
+        if(e.getItem().getType().equals(Material.COMPASS)){
+            if(e.getAction().equals(Action.RIGHT_CLICK_AIR)){
+
+                // When Right Clicking Compass - give nearest player
+
+            }
         }
-        return target;
     }
 }
