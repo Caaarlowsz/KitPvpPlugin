8c44b943ac77732134e599e71a8d0d88b6e4c86c
vscala <vincent2000nc@gmail.com>
2021-03-23 15:35:48 -0400

    Started work on custom items (added stub class)

--- src/main/java/me/finneganmcguire/kit_pvp_minecraft/GameLogic/GameVariables.java
+++ src/main/java/me/finneganmcguire/kit_pvp_minecraft/GameLogic/GameVariables.java
@@ -1,6 +1,11 @@
 package me.finneganmcguire.kit_pvp_minecraft.GameLogic;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class GameVariables {
@@ -15,7 +20,21 @@ public class GameVariables {
         public static int MAXZ = 0;
     }
     public static class CustomItems {
-        public static ItemStack GlassBow;
+        public static class GlassBow {
+            public static ItemStack getGlassBow() {
+                ItemStack glassBow = new ItemStack(Material.BOW);
+                return glassBow;
+            }
+            @EventHandler
+            public void onRightClick (PlayerInteractEvent event) {
+                Player p = event.getPlayer();
+                if (event.getAction() == Action.RIGHT_CLICK_AIR) {
+                    if (event.getItem().getType() == Material.BOW) {
+                        // insert logic
+                    }
+                }
+            }
+        }
         public static ItemStack GlassSword;
         public static ItemStack FeastSoup;
     }
